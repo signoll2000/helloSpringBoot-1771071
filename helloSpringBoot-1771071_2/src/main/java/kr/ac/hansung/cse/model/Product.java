49d952db535eed44ada8c2672d8d437488a4f0be
signoll2000 <50727968+signoll2000@users.noreply.github.com>
2020-06-26 16:29:34 +0900

    웹 프레임워크2 Homework #3

---
+++ helloSpringBoot-1771071_2/src/main/java/kr/ac/hansung/cse/model/Product.java
@@ -0,0 +1,59 @@
+package kr.ac.hansung.cse.model;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+
+@Entity
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@Table(name="product_table")
+public class Product implements Serializable {
+	private static final long serialVersionUID = 4577239542252345341L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id")
+	private Long id;
+
+	@Column(name = "name")
+	private String name;
+	
+	@Column(name = "category")
+	private String category;
+	
+	@Column(name = "price")
+	private int price;
+	
+	@Column(name = "manufacturer")
+	private String manufacturer;
+	
+	@Column(name = "unitinstock")
+	private int unitinstock;
+	
+	@Column(name = "description")
+	private String description;
+	
+
+	public Product(String name, String category, int price, String manufacturer, int unitinstock, String description) {
+		this.name = name;
+		this.category = category;
+		this.price = price;
+		this.manufacturer = manufacturer;
+		this.unitinstock = unitinstock;
+		this.description = description;
+	}
+}
